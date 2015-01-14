5edfcaabe8d5aa196cba62d5584e7f7869ffd5d4
WU Hualiang <wu_hu@mail.worksap.com>
2015-01-14 15:17:07 +0800

    Add gitignore

--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -n $1 ]; then
+if [ -z "$1" ]; then
     echo Usage: $0 IMAGE
     exit 1
 fi
 
-docker -d -t -v .:/var/data $1 bash -c "\
+docker run -d -t -v $PWD:/var/data $1 bash -c "\
     /usr/local/nginx/sbin/nginx -c /var/data/conf/nginx.conf && \
     /opt/redis-stable/src/redis-server /var/data/conf/redis.conf && \
-    echo start comment plugin"
+    sleep $(date +%s)"
